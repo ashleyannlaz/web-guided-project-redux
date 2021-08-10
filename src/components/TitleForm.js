45cc1e435392066889c4fc4a5cae25fd72b5780e
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-10 13:09:03 -0400

    lecture work-in-progress

--- src/components/TitleForm.js
+++ src/components/TitleForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { updateTitle} from './../actions/titleActions';
+import { updateTitle } from './../actions/titleActions';
 
 const TitleForm = (props) => {
   //Component State
@@ -11,7 +11,7 @@ const TitleForm = (props) => {
   };
 
   const handleSubmit = ()=> {
-
+    props.dispatch(updateTitle(newTitleText));
       // props.handleTitleUpdate(newTitleText);
   }
   
