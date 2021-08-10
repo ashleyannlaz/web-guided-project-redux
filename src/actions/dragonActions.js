83fc032682d60e4ba7e3fdd69b8d488284b54259
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-08-10 13:31:40 -0400

    lecture work-in-progress

---
+++ src/actions/dragonActions.js
@@ -0,0 +1,2 @@
+//1. create and export an action constant for ADD_NEW_MEMBER.
+//2. create and export an action creator for addNewMember.
--- src/reducers/dragonReducer.js
+++ src/reducers/dragonReducer.js
@@ -1,4 +1,6 @@
 //1. create and export initialState
 //2. add member into our initialState
 //3. create a reducer function
-//4. 
+//4. add in case ADD_NEW_MEMBER
+//5. add in a default case
+//6. export out reducer
